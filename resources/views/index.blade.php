6ba1e9cad42fc3a47e73c518a58445c70dc72f90
yusuf <idmahmad50@gmail.com>
2025-06-14 11:31:03 +0700

    version 1.0.3

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FORMADIKSI</title>
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('logopolindra.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('mascot.png') }}">
 
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -11,7 +11,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500;600&family=Roboto&display=swap" rel="stylesheet">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('logopolindra.png ') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('mascot.png ') }}">
 
     <!-- Icon Font Stylesheet -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" />
